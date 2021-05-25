130887125dfab7c83a456af3906775b49e9c7be4
fgoingtob <taranjot.s.ramana@gmail.com>
2021-05-25 00:45:52 -0400

    small_fun_apps

---
+++ madladlibs.py
@@ -0,0 +1,13 @@
+#This is a code to write a madlibs. 
+#if you want to know what madlibs is - https://en.wikipedia.org/wiki/Mad_Libs
+#this is achived by string concatinantion
+
+word1 = input("w1: ")
+word2 = input("w2: ")
+word3 = input("w3: ")
+celeb = input("name: ")
+madladlib = f"this is test to check {word1} !, I am so{word2}.Man {word3}\
+is stupid and{celeb} is so hot"
+
+print (madladlib)
+

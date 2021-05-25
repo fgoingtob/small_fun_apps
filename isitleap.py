c58da3e22f3d48ea37e83041bde11ad5f27d2919
fgoingtob <taranjot.s.ramana@gmail.com>
2021-05-25 00:45:44 -0400

    small_fun_apps

---
+++ isitleap.py
@@ -0,0 +1,8 @@
+#this is a small app to find if any year is leap of not
+year = int(input("what year you want to check: ")
+#now we define the conditon where we find out if it is a leap year or not
+if year % 4 == 
+	print (format.(year) "is a leap year")
+ else
+	print (format.(year) "is not a leap year")
+
